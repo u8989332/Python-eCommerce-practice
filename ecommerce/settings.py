55bd7cfe86ee3c7a276eb5e9fc4d3881ceb2fa16
Poseidon Gao <poseidongao@mail.liming360.com>
2018-06-06 16:12:46 +0800

    Setup & Serve Local Static & Media Files special bug: css need type="text/css" .....

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -117,4 +117,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static_my_proj")
+]
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
