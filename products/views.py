5e7056b90ef95b63fc402368d7204427b7625f56
Poseidon Gao <poseidongao@mail.liming360.com>
2018-06-07 10:23:50 +0800

    understanding lookup - can use python manage.py shell to filter objects

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, Http404
 
 # Create your views here.
 
@@ -33,7 +33,22 @@ class ProductDetailView(DetailView):
 
 def product_detail_view(request, pk=None, *args, **kwargs):
     #instance = Product.objects.all()
-    instance = get_object_or_404(Product, pk=pk)
+    #instance = get_object_or_404(Product, pk=pk)
+
+    # try:
+    #     instance = Product.objects.get(id=pk)
+    # except Product.DoesNotExist:
+    #     print('no product here')
+    #     raise Http404("Product doesn't exist")
+    # except:
+    #     print("???? h?")
+
+    qs = Product.objects.filter(id=pk)
+    if qs.exists() and qs.count() == 1:
+        instance = qs.first()
+    else:
+        raise Http404("Product doesn't exist")
+
     context = {
         'object': instance
     }
